e1e146fac855003253e3d269cfefd5054e0d1353
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-20 21:17:00 -0600

    add, delete and show functions

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,40 +1,34 @@
 import React, { useState } from 'react';
 import WorkspaceModal from '../../components/WorkspaceModal/index';
 import QRModal from '../../components/QRModal';
+import { UseImages } from '../../utils/customHooks/useImages';
 import './styles.scss';
 
 const Home = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [generatedQR, setGeneratedQR] = useState<boolean>(false);
-  const [images, setImages] = useState<any>([]);
+  const {
+    upload,
+    setGeneratedQR,
+    setImages,
+    images,
+    generatedQR,
+    setIsLoading,
+  } = UseImages();
 
-  const readAll = (files) => {
-    return [...files].map(file => new Promise((resolve, reject) => {
-      const reader: any = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result)
-    }))
-  }
-
-  const upload = (e:any) => {
-    const files = e.target.files;
-    return Promise.all(readAll(files))
-      .then((images) => setImages(images))
-  }
-
-  const handleUpload = async (e:any) => {
-    await setShowModal(true);
+  const handleUpload = async (e: any) => {
+    setShowModal(true);
     await upload(e);
-  }
+  };
 
-  const handleCloseModal = (e:boolean) => {
+  const handleCloseModal = (e: boolean) => {
     setShowModal(false);
     setGeneratedQR(false);
-    setImages([])
+    setImages([]);
   }
-  const handleOpenQRModal = (e:boolean) => {
-    setGeneratedQR(true)
-  }
+
+  const handleOpenQRModal = (e: boolean) => {
+    setGeneratedQR(true);
+  };
 
   return (
     <section className="upload">
@@ -46,26 +40,26 @@ const Home = () => {
           onChange={handleUpload}
           type="file"
         />
-        { showModal &&
-          <div className="modal fadeIn" >
+        {showModal && (
+          <div className="modal fadeIn">
             <WorkspaceModal
               isOpen={handleUpload}
               onClose={handleCloseModal}
               handleEvent={handleOpenQRModal}
-              files = {images}
+              files={images}
             />
-            { generatedQR === true && (
+            {generatedQR === true && (
               <QRModal
                 isOpen={generatedQR}
                 GeneratedUrl="here must be the url"
               />
             )}
           </div>
-        }
+        )}
       </div>
       <p>Only jpg, png, gif, svg</p>
     </section>
-  )
-}
+  );
+};
 
 export default Home;
