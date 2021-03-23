ccfd6f5b85e577d0d843416403d7a9c3f4949ab8
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-23 10:23:04 -0600

    fix bugs with back implementations

--- src/utils/api/images.ts
+++ src/utils/api/images.ts
@@ -1,7 +1,7 @@
 export const uploadImages = async (rawData: any) => {
 	const formdata = new FormData();
 	rawData.forEach((element: any) => {
-		formdata.append('images', element);
+		formdata.append('images', element,'name');
 	});
 
 	const response = await fetch('https://www.pictocode.xyz/api/v1/images/', {
@@ -14,17 +14,16 @@ export const uploadImages = async (rawData: any) => {
 };
 
 export const downloadImages = async (uid: string) => {
-	fetch(`https://www.pictocode.xyz/api/v1/images/${uid}`, {
-		method: 'GET',
+	var formdata = new FormData();
+
+	fetch(`https://pictocode.xyz/api/v1/images/${uid}`, {
+		method: 'POST',
+		body: formdata,
 		redirect: 'follow',
 	})
 		.then((response) => response.text())
 		.then((result) => console.log(result))
 		.catch((error) => console.log('error', error));
-
-	// const response = await fetch(
-	// 	`https://www.pictocode.xyz/api/v1/images/${uid}`,
-	// ).then((response) => console.log(response.json()));
 
 	// return response;
 };
